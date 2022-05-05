79634da16ab0754cada546cbd7d9f171f042fd8a
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2022-05-05 12:43:29 +0100

    resolved form ish

--- resources/views/backend/sports/addsports.blade.php
+++ resources/views/backend/sports/addsports.blade.php
@@ -18,12 +18,13 @@
                             <option value="">Select type</option>
                             <option value="Compulsory">Compulsory</option>
                             <option value="Optional">Optional</option>
+                        </select>
                     </div>
 
 
-                    <div class="col"><br>
+                    <div class="mb-3">
                         <label class="form-label">Sport</label>
-                        <input type="text" class="form-control" name="">
+                        <input type="text" class="form-control" name="" placeholder="Sports Name">
                     </div>
 
                     <div class="mb-3">
@@ -43,49 +44,33 @@
                     <table id="example2" class="table table-striped table-bordered">
                         <thead>
                             <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
+                                <th>Sn</th>
+                                <th>Sport</th>
+                                <th>Sport Category</th>
+
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <td>Tiger Nixon</td>
-                                <td>System Architect</td>
-                                <td>Edinburgh</td>
-                                <td>61</td>
-                                <td>2011/04/25</td>
-                                <td>$320,800</td>
+                                <td>1</td>
+                                <td>Badminton</td>
+                                <td>Compulsory</td>
+
                             </tr>
+
                             <tr>
-                                <td>Garrett Winters</td>
-                                <td>Accountant</td>
-                                <td>Tokyo</td>
-                                <td>63</td>
-                                <td>2011/07/25</td>
-                                <td>$170,750</td>
+                                <td>2</td>
+                                <td>Athletics</td>
+                                <td>Compulsory</td>
+
                             </tr>
-                            <tr>
-                                <td>Ashton Cox</td>
-                                <td>Junior Technical Author</td>
-                                <td>San Francisco</td>
-                                <td>66</td>
-                                <td>2009/01/12</td>
-                                <td>$86,000</td>
-                            </tr>
-                            
+
                         </tbody>
                         <tfoot>
                             <tr>
-                                <th>Name</th>
-                                <th>Position</th>
-                                <th>Office</th>
-                                <th>Age</th>
-                                <th>Start date</th>
-                                <th>Salary</th>
+                                <th>Sn</th>
+                                <th>Sport</th>
+                                <th>Sport Category</th>
                             </tr>
                         </tfoot>
                     </table>
